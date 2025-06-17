526900122de2bd0b7a7f9f86142cdb5379d17367
Saravanan-e-2003 <motts2003@gmail.com>
2025-06-17 23:24:46 +0530

    Added category display

--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -3,11 +3,16 @@ import { useEffect, useState } from "react";
 // import '../style.css'
 function QuoteBox(){
     const[quote,setQuote] = useState("Loading....");
+    const[txtCategory,setTxtCategory] = useState("");
+    const[category,setCategory] = useState("");
 
     const generate = async () => {
-        let data = await fetch(`https://rizzapi.vercel.app/random`);
+        let params = category == ""?`random`:`category/${category}`;
+        let data = await fetch(`https://rizzapi.vercel.app/${params}`);
+        // console.log(`https://rizzapi.vercel.app/${params}`);
         let response = await data.json();
         setQuote(response?.text ?? "Loading....");
+        setTxtCategory(response?.category?? "");
     }
 
     useEffect(() =>{
@@ -18,6 +23,8 @@ function QuoteBox(){
         <div className="wrapper">
             <div className="quote-box">
                 <p id="txt">{quote}</p>
+               <p id = "line">_______________________________________</p>
+                <p id= "categories">category:-{txtCategory}</p>
                 <button className="generate-button" onClick={generate}>Generate</button>
             </div>
         </div>
